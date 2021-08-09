8c2280ddee4f3393102105cb70ca1753eaa75eb8
osapsai <51738844+osapsai@users.noreply.github.com>
2021-08-09 21:13:15 +0300

    Update terraform_azure.tf

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -133,7 +133,7 @@ resource "azurerm_storage_account" "mystorageaccount" {
     name                        = "diag${random_id.randomId.hex}"
     resource_group_name         = azurerm_resource_group.myterraformgroup.name
     location                    = azurerm_resource_group.myterraformgroup.location
-    account_tier                = "Premium"
+    account_tier                = "Standard"
     account_replication_type    = "LRS"
 
     tags = {
