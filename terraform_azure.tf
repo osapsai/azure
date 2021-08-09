4b593f932fd9e6f7fa2d1dae9dcfaa482c092009
osapsai <51738844+osapsai@users.noreply.github.com>
2021-08-09 19:37:15 +0300

    Update terraform_azure.tf

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -10,7 +10,12 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
+variable "mylocation" {
+  type = string
+}
+variable "ssh-key" {
+  type = string
+}
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "myResourceGroup"
